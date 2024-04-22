17973144c5f7ced47d0c4baaba777b66999b7911
nate smith <nateksmith@pm.me>
2024-04-21 20:03:36 -0700

    minor cleanup

--- ingest_gutenberg.sh
+++ ingest_gutenberg.sh
@@ -4,5 +4,8 @@ p="/home/vilmibm/pg_plaintext/files/"
 
 while read book; do
   title=`cat $p$book | guttitle.sh`
-  cat $p$book | gutcontent | phraser | gutingest "$book $title"
+  cat $p$book \
+    | gutcontent \
+    | phraser \
+    | gutingest "$book $title"
 done
