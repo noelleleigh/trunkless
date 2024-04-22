0e114bc448701cba461832569521e231a6ad4880
nate smith <nateksmith@pm.me>
2024-04-21 20:03:20 -0700

    strip quote characters from gutenberg book titles

--- guttitle.sh
+++ guttitle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep "*** START OF THE PROJECT GUTENBERG" | sed 's/^\*\*\* START OF THE PROJECT GUTENBERG EBOOK //' | sed 's/\*\*\*//'
+grep "*** START OF THE PROJECT GUTENBERG" | sed 's/^\*\*\* START OF THE PROJECT GUTENBERG EBOOK //' | sed 's/\*\*\*//' | tr -d "\"'"
