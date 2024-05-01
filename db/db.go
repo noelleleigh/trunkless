25147572f8192ad15af9ecefd844f28f89470062
nate smith <nateksmith@pm.me>
2024-04-30 21:19:19 -0700

    add db.Pool

--- db/db.go
+++ db/db.go
@@ -6,12 +6,13 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
+	// TODO unused; i think i failed to make this work. should i just rely on the env
+	// vars like i've been doing?
 	dburl = "postgresql://vilmibm/postgres?host=/home/vilmibm/src/trunkless/pgdata/sockets"
-	//	MaxID = 467014991
-	MaxID = 345507789
 )
 
 func StrToID(s string) string {
@@ -25,6 +26,15 @@ func Connect() (*pgx.Conn, error) {
 	}
 
 	return conn, nil
+}
+
+func Pool() (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), "")
+	if err != nil {
+		return nil, fmt.Errorf("Unable to connect to database: %w", err)
+	}
+
+	return pool, nil
 }
 
 // TODO func for getting ID ranges for each corpus in phrases
